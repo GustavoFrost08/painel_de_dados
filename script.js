2c7cab35ed544cda7110f72071b55f721927cdd4
GustavoFrost08 <gustavo_frost@estudante.sesisenai.org.br>
2026-05-11 08:52:23 -0300

    pt2, complete project
    Co-authored-by: giovannabresciani <giovannabresciani@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
 function toggleInfo(botao) {
+  const card = botao.parentElement;
 
-    const card = botao.parentElement;
+  const info = card.querySelector(".info_extra");
 
-    const info = card.querySelector(".info_extra");
-
-    info.classList.add("show");
+  info.classList.add("show");
 }
 
 function fecharInfo(info) {
-
-    info.classList.remove("show");
+  info.classList.remove("show");
 }
 
 function toggleMenu() {
-    const sidebar = document.getElementById("sidebar");
-    sidebar.classList.toggle("open");
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.toggle("open");
 }
+
+/*grafico de pizza*/
+const ctx = document.getElementById("meuGrafico");
+
+new Chart(ctx, {
+  type: "pie",
+  data: {
+    labels: ["Mundo Aberto Imersivo", "RPG com Escolhas", "Gameplay Difícil/Desafiador", "História Emocional/Cinematográfica", "Puzzle/Inteligência", "Ambientação Futurista"],
+    datasets: [
+      {
+        data: [32, 24, 18, 15, 6, 5],
+        backgroundColor: ["#ff0000", "#1c00ff", "#fff500", "#e418bd", "#19ff37", "#7415ed" ],
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    plugins: {
+        legend: {
+            labels: {
+                color: 'white',
+                font: {
+                    size: 14,
+                }
+            }
+        }
+    }
+  },
+});
